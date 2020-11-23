c2871439183fa055c58f16a77bdaeb29a5083c2f
UYasher <shriyash.upadhyaya@gmail.com>
2020-11-23 15:38:30 -0500

    Added headers and tests for solvers

--- src/FactoryEditing.hs
+++ src/FactoryEditing.hs
@@ -1,10 +1,13 @@
 module FactoryEditing where
 
+import qualified Data.Map as Map
+import Data.Maybe (isJust)
 import Geometry
 import Machine
-import qualified Data.Map as Map
 
 data Factory = Factory {permanentFloor :: Grid Machine, editableFloor :: Grid Machine, width :: Int, height :: Int}
+
+-- Operations to Edit Factories
 
 blankFactory :: Int -> Int -> Factory
 blankFactory = Factory Map.empty Map.empty
@@ -24,4 +27,47 @@ placeMachineAt = undefined
 removeMachineAt :: Point -> Factory -> Factory
 removeMachineAt = undefined
 
+-- Operations to step factories
 
+step :: Factory -> Factory
+step = undefined
+
+stepN :: Factory -> Int -> Factory
+stepN f 0 = f
+stepN f n = iterate step f !! n
+
+-- Operations to solve factories
+
+type Solver = Factory -> Maybe (Factory, Int)
+
+-- Check if a factory is solved
+isSolved :: Factory -> Bool
+isSolved = undefined
+
+-- Any Int puzzle can be solved by going to a prime, factoring, producing a one, and then repeatedly adding
+-- There is a minimum amount of space required for that structure
+-- If such a structure exists, return the solution factory and the number of steps required to reach a solution
+-- Note it might be possible to come up with a better simple solution with BFS on a dependency graph
+findSimpleSolution :: Solver
+findSimpleSolution = undefined
+
+isSimpleSolvable :: Factory -> Bool
+isSimpleSolvable f = isJust $ findSimpleSolution f
+
+-- Some problems can be solved more efficiently than the simple solution
+-- It might be possible to improve those solutions using local search
+-- The generated solution might not be optimal
+findComplexSolution :: Solver
+findComplexSolution = undefined
+
+isComplexSolvable :: Factory -> Bool
+isComplexSolvable f = isJust $ findComplexSolution f
+
+-- Generates the solution to a puzzle which uses the fewest machines
+-- If local search (or other alternatives for implementing findComplexSolution)
+-- Do not return an optimal solution, then we may use constraint satisfaction
+findOptimalSolution :: Solver
+findOptimalSolution = undefined
+
+isSolvable :: Factory -> Bool
+isSolvable f = isJust $ findOptimalSolution f
--- test/FactoryEditingTests.hs
+++ test/FactoryEditingTests.hs
@@ -3,6 +3,18 @@ import Test.QuickCheck
 import FactoryEditing
 import Data.Map as Map
 
+-- Need arbitrary instance for factories -- need to be sophisticated about this
+instance Arbitrary Factory where
+    arbitrary = undefined
+    shrink = undefined
+
+-- Need arbitrary instance for machines -- this will be a choose, might need to be a little smarter about shrink
+instance Arbitrary Machine where
+    arbitrary = undefined
+    shrink = undefined
+
+
+-- Tests to check factory editing operations
 prop_place :: Point -> Machine -> Factory -> Bool
 prop_place p m f = Map.lookup p (placeMachineAt p f m) == Just m
 
@@ -40,12 +52,36 @@ prop_removeRotate p o = removeMachineAt p . rotateMachineAt p o == rotateMachine
 prop_displaceRotate :: Point -> Point -> Orientation -> Bool
 prop_displaceRotate p1 p2 o = displaceMachineAt p1 p2 (rotateMachine o) == rotateMachineAt (p1 + p2) o (displaceMachineAt p1 p2)
 
--- Need arbitrary instance for factories -- need to be sophisticated about this
-instance Arbitrary Factory where
-    arbitrary = undefined
-    shrink = undefined
 
--- Need arbitrary instance for machines -- this will be a choose 
-instance Arbitrary Machine where
-    arbitrary = undefined
-    shrink = undefined
+-- Tests to check factory solvability
+
+prop_solutionIsSolvable :: Solver -> Factory -> Bool
+prop_solutionIsSolvable p f = case p f of
+    Nothing -> True
+    Just (f' n) -> isSolved (stepN f' n)
+
+prop_simpleSolutionIsSolvable :: Factory -> Bool
+prop_simpleSolutionIsSolvable = prop_solutionIsSolvable findSimpleSolution
+
+prop_copmlexSolutionIsSolvable :: Factory -> Bool
+prop_copmlexSolutionIsSolvable = prop_solutionIsSolvable findComplexSolution
+
+prop_optimalSolutionIsSolvable :: Factory -> Bool
+prop_copmlexSolutionIsSolvable = prop_solutionIsSolvable findOptimalSolution
+
+-- Establishes a partial ordering on solvers
+(*<=) :: Solver -> Solver -> (Factory -> Bool)
+s1 (*<=) s2 = \f -> case s2 f of
+    Nothing -> True
+    Just (_, n2) -> case s1 of
+        Nothing -> False
+        Just (_, n1) -> n2 <= n1
+
+prop_complexLESimple :: Factory -> Bool
+prop_complexLessThanSimple = findComplexSolution *<= findSimpleSolution
+
+prop_optimalLESimple :: Factory -> Bool
+prop_optimalLessThanSimple = findOptimalSolution *<= findSimpleSolution
+
+prop_optimalLEComplex :: Factory -> Bool
+prop_complexLessThanSimple = findOptimalSolution *<= findComplexSolution
