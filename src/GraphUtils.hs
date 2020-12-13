e6b03f71bff62293d826130d3865b0c84323c45d
UYasher <shriyash.upadhyaya@gmail.com>
2020-12-13 17:26:49 -0500

    Refactored Graph to Map Int (Set Int)

--- src/GraphUtils.hs
+++ src/GraphUtils.hs
@@ -10,14 +10,7 @@ import Machine
 import Wire
 
 -- | A Graph is a map from a node to its outNeighbors
-newtype Graph = Graph (Map.Map Int [Int]) deriving (Show)
-
--- | A notion of graph equality
--- | Checks for equality ignoring the order in which a nodes outNeighbors appear
-instance Eq Graph where
-  Graph m1 == Graph m2 = m1 `containsUnordered` m2 && m2 `containsUnordered` m1
-    where
-      containsUnordered a b = all (\x -> (Set.fromList <$> Map.lookup x a) == (Set.fromList <$> Map.lookup x b)) (Map.keys b)
+newtype Graph = Graph (Map.Map Int (Set Int)) deriving (Show, Eq)
 
 pointToInt :: Blueprint -> Point -> Int
 pointToInt Blueprint {width = w} Point {pointX = x, pointY = y} = x + y * w
@@ -30,7 +23,7 @@ intToPoint Blueprint {width = w} n = Point (n `safeMod` w) (n `div` w)
 blueprintToGraph :: Blueprint -> Graph
 blueprintToGraph b@Blueprint {grid = g} = Graph $ Map.fromList $ Prelude.map toEdge (allPoints b)
   where
-    toEdge p = (pointToInt b p, Prelude.map (pointToInt b) (openAdjacentPoints p))
+    toEdge p = (pointToInt b p, Set.fromList $ Prelude.map (pointToInt b) (openAdjacentPoints p))
     adjacentPoints p = Prelude.filter (`isInBounds` b) [p +>> Point 0 1, p +>> Point 1 0, p ->> Point 0 1, p ->> Point 1 0]
     isOpen p p' = case Map.lookup p' g of
       Nothing -> True
@@ -42,24 +35,22 @@ blueprintToGraph b@Blueprint {grid = g} = Graph $ Map.fromList $ Prelude.map toE
     openAdjacentPoints p = Prelude.filter (isOpen p) (adjacentPoints p)
 
 -- | Run a bfs on a tree, producing a map from children to parents
--- | To start bfs on a graph `g` from a node `n` run bfs g [n] Map.empty
--- | Because of this convention, the root node will have a self edge in the Map
 --     graph     queue   parents        updated parents
 bfs :: Graph -> [Int] -> Map Int Int -> Map Int Int
-bfs g [x] parents | parents == Map.empty = bfs g [x] (Map.insert x x Map.empty)
 bfs _ [] parents = parents
 bfs g xs parents = bfs g xs' parents'
   where
-    -- Using nub here is inefficient, there should be a better way probably using fold
-    xs' = nub $ Prelude.filter (`Map.notMember` parents) $ children g xs
+    xs' = Prelude.filter (`Map.notMember` parents) $ children g xs
     parents' = Prelude.foldr addChildrenToParents parents xs
     addChildrenToParents x m = Prelude.foldr (`insertIfNew` x) m (children g [x])
 
+bfsFrom :: Graph -> Int -> Map Int Int
+bfsFrom g x = bfs g [x] Map.empty
+
 -- | Given a graph and a list of nodes,
 -- | return the list of all children of nodes in the list.
--- | Includes duplicate children
 children :: Graph -> [Int] -> [Int]
-children (Graph m) ys = concat (Maybe.mapMaybe (`Map.lookup` m) ys)
+children (Graph m) ys = Set.toList $ Set.unions (Maybe.mapMaybe (`Map.lookup` m) ys)
 
 -- | Utility to insert a pair into a map iff the key doesn't exist
 insertIfNew :: Ord k => k -> a -> Map k a -> Map k a
